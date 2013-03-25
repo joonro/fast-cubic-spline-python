5ddc888da712f78b3bd72cda8ec6f94cf3ded9e8
Joon Ro <joonhyoung.ro@gmail.com>
2013-03-24 21:07:06 -0500

    removed other modules from setup.py

--- setup.py
+++ setup.py
@@ -5,33 +5,10 @@ import os
 import numpy
 
 ext_modules = []
-ext_modules.append(Extension(
-    "chebyshev",
-    ["chebyshev.pyx"],
-    libraries=["m"],
-    extra_compile_args=['-fopenmp'],
-    extra_link_args=['-fopenmp'],
-))
 
 ext_modules.append(Extension(
     "_spline",
     ["spline.pyx"],
-    libraries=["m"],
-    extra_compile_args=['-fopenmp'],
-    extra_link_args=['-fopenmp'],
-))
-
-ext_modules.append(Extension(
-    "_pchip",
-    ["_pchip.pyx"],
-    libraries=["m"],
-    extra_compile_args=['-fopenmp'],
-    extra_link_args=['-fopenmp'],
-))
-
-ext_modules.append(Extension(
-    "_rbf",
-    ["_rbf.pyx"],
     libraries=["m"],
     extra_compile_args=['-fopenmp'],
     extra_link_args=['-fopenmp'],
